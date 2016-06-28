163be02162da63af85ae33e2815a88eaef9cb914
LiangGuanyu <lianggy0719@126.com>
2016-06-28 11:25:23 +0800

    Use kNN to classify shop

--- generate_zone.py
+++ generate_zone.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import utils
+
 def do_mark_spots(conn):
     cu = conn.cursor()
     cu.execute("select site_id, spot_id from eb_order")
@@ -9,17 +13,36 @@ def do_mark_spots(conn):
     conn.commit()
 
 def do_classify_shops(conn):
-    pass
+    cu = conn.cursor()
+    X = []
+    y = []
+    for lng, lat, zone in cu.execute("select lng, lat, zone from spot"):
+        X.append((lng, lat))
+        y.append(zone)
+    neigh = KNeighborsClassifier(n_neighbors=5, metric='pyfunc', func=utils.distance)
+    neigh.fit(np.array(X), np.array(y))
 
-def do_generate(conn):
-    do_mark_spots(conn)
-    do_classify_shops(conn)
+    zones = []
+    for shop_id, lng, lat in cu.execute("select shop_id, lng, lat from shop"):
+        # print (shop_id, neigh.predict([[lng, lat]])[0])
+        zones.append((neigh.predict([[lng, lat]])[0], shop_id))
+    cu.executemany("update shop set zone=? where shop_id=?", zones)
+    conn.commit()
+
+def do_generate(conn, action):
+    if action == 'spot' or action == 'all':
+        do_mark_spots(conn)
+    if action == 'shop' or action == 'all':
+        do_classify_shops(conn)
 
 if __name__ == '__main__':
-    import sqlite3
+    import sqlite3, sys
+    if len(sys.argv) < 2:
+        print "Usage: generate_zone.py [spot|shop|all]"
+        sys.exit(1)
 
     conn = sqlite3.connect('./Data/data.db')
-    do_generate(conn)
+    do_generate(conn, sys.argv[1])
     conn.close()
 
 # vim: set sw=4 ts=4 softtabstop=4
